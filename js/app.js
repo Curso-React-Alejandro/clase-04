8f39680b6e36f8181c1b95203325cf17bcf6d872
Fernando Warno <ferwar@hotmail.com>
2022-08-29 04:42:30 -0300

    New classes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 // importo la clase Persona para poder usarla
 import Persona from './persona.js';
+import Perro from './perro.js';
+import Gato from './gato.js';
+import Animal from './animal.js';
+
 
 let myPerson = new Persona('Juan', 'Perez', 18, 1.72);
 
 console.log(myPerson.saludar());
+
+let myPerro = new Perro('Firulais', 'Pastor Aleman', 3);
+
+myPerro.ladrar();
